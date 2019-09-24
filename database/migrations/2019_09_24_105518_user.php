1e6fa002c181865b77bf354e6bed80b3ef6599ab
Iago Melo <iagosilvamelo@gmail.com>
2019-09-24 08:03:25 -0300

    Criado migration_users

---
+++ database/migrations/2019_09_24_105518_user.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class User extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $new) {
+            $new->bigIncrements('ID');
+            $new->string('username', 50);
+            $new->string('password', 100);
+            $new->rememberToken();
+            $new->integer('people_id');
+            $new->integer('type');
+            $new->char('status');
+            $new->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
